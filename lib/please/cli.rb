a6539718f2d4bc0f28446b42fe6f4f9218323ea2
Joe Anderson <joe@osbert.me>
2021-09-15 15:45:52 +0100

    Prevent stack trace on keyboard interrupt

--- lib/please/cli.rb
+++ lib/please/cli.rb
@@ -6,63 +6,67 @@ require 'optparse'
 require 'please'
 require 'tempfile'
 
-tty_prompt = TTY::Prompt.new
+begin
+  tty_prompt = TTY::Prompt.new
 
-options = {}
+  options = {}
 
-USAGE = 'Usage: please [options] <instruction>'
+  USAGE = 'Usage: please [options] <instruction>'
 
-OptionParser.new do |opts|
-  opts.banner = USAGE
-end.parse!
+  OptionParser.new do |opts|
+    opts.banner = USAGE
+  end.parse!
 
-access_token = ENV.fetch('OPENAI_ACCESS_TOKEN') do
-  tty_prompt.error 'Ensure the OPENAI_ACCESS_TOKEN environment variable is set'
-  exit 1
-end
-
-codex_service = Please::OpenAI::CodexService.new(access_token: access_token)
-
-instruction = ARGV.join(' ')
-
-if instruction.empty?
-  tty_prompt.error USAGE
-  exit 1
-end
-
-request = Please::Request.new(
-  options: options,
-  instruction: instruction,
-  codex_service: codex_service,
-)
-
-command = request.send
-
-loop do
-  print '$ '
-  tty_prompt.ok command
-
-  action = tty_prompt.expand('Run the command?') do |q|
-    q.choice key: 'y', name: 'Yes', value: :run
-    q.choice key: 'n', name: 'No', value: :abort
-    q.choice key: 'e', name: 'Edit command before running', value: :edit
+  access_token = ENV.fetch('OPENAI_ACCESS_TOKEN') do
+    tty_prompt.error 'Ensure the OPENAI_ACCESS_TOKEN environment variable is set'
+    exit 1
   end
 
-  case action
-  when :run
-    Process.wait spawn(command)
-    exit $CHILD_STATUS.exitstatus
-  when :abort
-    break
-  when :edit
-    Tempfile.open('command') do |file|
-      file << command
-      file.flush
+  codex_service = Please::OpenAI::CodexService.new(access_token: access_token)
 
-      Process.wait spawn("${EDITOR:-vi} #{file.path}")
+  instruction = ARGV.join(' ')
 
-      file.rewind
-      command = file.read.chomp
+  if instruction.empty?
+    tty_prompt.error USAGE
+    exit 1
+  end
+
+  request = Please::Request.new(
+    options: options,
+    instruction: instruction,
+    codex_service: codex_service,
+  )
+
+  command = request.send
+
+  loop do
+    print '$ '
+    tty_prompt.ok command
+
+    action = tty_prompt.expand('Run the command?') do |q|
+      q.choice key: 'y', name: 'Yes', value: :run
+      q.choice key: 'n', name: 'No', value: :abort
+      q.choice key: 'e', name: 'Edit command before running', value: :edit
+    end
+
+    case action
+    when :run
+      Process.wait spawn(command)
+      exit $CHILD_STATUS.exitstatus
+    when :abort
+      break
+    when :edit
+      Tempfile.open('command') do |file|
+        file << command
+        file.flush
+
+        Process.wait spawn("${EDITOR:-vi} #{file.path}")
+
+        file.rewind
+        command = file.read.chomp
+      end
     end
   end
+rescue Interrupt
+  exit 130
 end
