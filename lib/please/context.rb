1f4ba0bbb679313293f4795b5797c3e2310af8f9
Joe Anderson <joe@osbert.me>
2021-09-16 12:26:04 +0100

    Add examples of how to escape % and \ within printf

--- lib/please/context.rb
+++ lib/please/context.rb
@@ -4,31 +4,31 @@ DEFAULT_EXAMPLES = [
   {
     instruction: 'Find all files older than 1 week and open each of them in vim',
     command: 'find . -type f -mtime +7 -exec vim {} \;',
-    result: '',
   },
 
   {
-    instruction: 'Download a random dog picture',
-    command: 'printf \'import urllib.request\nimport json\nimport subprocess\ndata = urllib.request.urlopen("https://dog.ceo/api/breeds/image/random").read()\nurl = json.loads(data)["message"]\nsubprocess.call(["curl", url, "-o", "dog.jpg"])\' > /tmp/program.py; python3 /tmp/program.py; rm /tmp/program.py',
-    result: '',
+    instruction: 'Show a clock which updates every second on a single line',
+    command: 'printf \'import time\nwhile True:\n\tprint(time.strftime("%%H:%%M:%%S"), end="\\\\r")\n\ttime.sleep(1)\' > /tmp/program.py; python3 /tmp/program.py; rm /tmp/program.py',
+  },
+
+  {
+    instruction: 'Write a python program that prints out hello world with each letter on its own line',
+    command: 'printf \'print("\\\\n".join(list("Hello World"))\' > /tmp/program.py; python3 /tmp/program.py; rm /tmp/program.py',
   },
 
   {
     instruction: 'Read from stdin until EOF, and then output the length of the string',
     command: 'printf \'import sys\nprint(len(sys.stdin.read().strip()))\' > /tmp/program.py; python3 /tmp/program.py; rm /tmp/program.py',
-    result: '',
   },
 
   {
     instruction: 'Run the fortune command 5 times',
     command: 'for i in $(seq 5); do fortune; done',
-    result: '',
   },
 
   {
     instruction: 'Repeadedly read a single line from the user, reverse it, and print it back',
     command: 'printf \'while True:\n\tline = input()\n\tprint(line[::-1])\' > /tmp/program.py; python3 /tmp/program.py; rm /tmp/program.py',
-    result: '',
   },
 ].freeze
 
@@ -36,16 +36,19 @@ OPTIONAL_EXAMPLES = {
   pwd: {
     instruction: 'Print the current working directory',
     command: 'pwd',
+    execute: true,
   },
 
   uname: {
     instruction: 'Show information about the operating system',
     command: 'uname -a',
+    execute: true,
   },
 
   ls: {
     instruction: 'List all files in the current directory',
     command: 'ls -a',
+    execute: true,
   },
 }.freeze
 
@@ -64,7 +67,7 @@ module Please
         <<~EXAMPLE.chomp
           # #{example[:instruction]}
           $ #{example[:command]}
-          #{example.fetch(:result) { `#{example[:command]}` }}
+          #{if example[:execute] then `#{example[:command]}` else '' end}
         EXAMPLE
       end.join("\n")
     end
